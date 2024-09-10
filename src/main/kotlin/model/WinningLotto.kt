59df468ab83edde807aa93bd97cf36e4346d5a05
임현호 <limhh0801@hyundai.com>
2024-09-10 11:08:27 +0900

    feat: 로또 등수를 정할 수 있다.

--- src/main/kotlin/model/WinningLotto.kt
+++ src/main/kotlin/model/WinningLotto.kt
@@ -8,4 +8,20 @@ class WinningLotto(
         require(!lotto.contains(bonus))
     }
 
+    fun rank(lotto: Lotto): Int {
+        val match = this.lotto.intersect(lotto).size
+        val matchBonus = lotto.contains(bonus)
+
+        return rank(match, matchBonus)
+    }
+
+    private fun rank(match: Int, bonus: Boolean): Int = when {
+        match == 6 -> 1
+        match == 5 && bonus -> 2
+        match == 5 && !bonus -> 3
+        match == 4  -> 4
+        match == 3  -> 5
+        else -> 6
+    }
+
 }
--- src/test/kotlin/model/WinningLottoTest.kt
+++ src/test/kotlin/model/WinningLottoTest.kt
@@ -22,4 +22,46 @@ class WinningLottoTest {
         assertThrows<IllegalArgumentException> { WinningLotto(lotto, bonus) }
     }
 
+    @Test
+    fun `1등 로또를 확인할 수 있다`() {
+        val lotto = Lotto(setOf(1,2,3,4,5,6))
+        val winningLotto = WinningLotto(Lotto(setOf(1,2,3,4,5,6)), 7)
+        assertThat(winningLotto.rank(lotto)).isEqualTo(1)
+    }
+
+    @Test
+    fun `2등 로또를 확인할 수 있다`() {
+        val lotto = Lotto(setOf(1,2,3,4,5,7))
+        val winningLotto = WinningLotto(Lotto(setOf(1,2,3,4,5,6)), 7)
+        assertThat(winningLotto.rank(lotto)).isEqualTo(2)
+    }
+
+    @Test
+    fun `3등 로또를 확인할 수 있다`() {
+        val lotto = Lotto(setOf(1,2,3,4,5,8))
+        val winningLotto = WinningLotto(Lotto(setOf(1,2,3,4,5,6)), 7)
+        assertThat(winningLotto.rank(lotto)).isEqualTo(3)
+    }
+
+    @Test
+    fun `4등 로또를 확인할 수 있다`() {
+        val lotto = Lotto(setOf(1,2,3,4,7,8))
+        val winningLotto = WinningLotto(Lotto(setOf(1,2,3,4,5,6)), 7)
+        assertThat(winningLotto.rank(lotto)).isEqualTo(4)
+    }
+
+    @Test
+    fun `5등 로또를 확인할 수 있다`() {
+        val lotto = Lotto(setOf(1,2,3,7,9,8))
+        val winningLotto = WinningLotto(Lotto(setOf(1,2,3,4,5,6)), 7)
+        assertThat(winningLotto.rank(lotto)).isEqualTo(5)
+    }
+
+    @Test
+    fun `6등 로또를 확인할 수 있다`() {
+        val lotto = Lotto(setOf(1,2,8,9,10,11))
+        val winningLotto = WinningLotto(Lotto(setOf(1,2,3,4,5,6)), 7)
+        assertThat(winningLotto.rank(lotto)).isEqualTo(6)
+    }
+
 }
